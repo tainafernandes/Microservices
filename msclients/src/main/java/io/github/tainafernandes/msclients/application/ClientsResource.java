24be161cba04346b1ee4a6ff5dc25384f5f8abd4
tainafernandes <tainafefe@gmail.com>
2023-01-16 14:30:25 -0300

    Implementing the Gateway

--- msclients/src/main/java/io/github/tainafernandes/msclients/application/ClientsResource.java
+++ msclients/src/main/java/io/github/tainafernandes/msclients/application/ClientsResource.java
@@ -4,6 +4,7 @@ import io.github.tainafernandes.msclients.application.representation.ClientSaveR
 import io.github.tainafernandes.msclients.domain.Client;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
+@Slf4j
 @RestController
 @RequestMapping("clients")
 @RequiredArgsConstructor
@@ -20,6 +21,7 @@ public class ClientsResource {
     private final ClientService service;
     @GetMapping
     public String status(){
+        log.info("Getting microservice status from clients");
         return "ok";
     }
     @PostMapping
---
+++ mscloudgateway/src/main/java/io/github/tainafernandes/mscloudgateway/MscloudgatewayApplication.java
@@ -0,0 +1,26 @@
+package io.github.tainafernandes.mscloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@EnableEurekaClient
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MscloudgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscloudgatewayApplication.class, args);
+	}
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder){
+		return builder
+				.routes()
+					.route(r -> r.path("/clients/**").uri("lb://msclients"))
+				.build();
+	}
+}
