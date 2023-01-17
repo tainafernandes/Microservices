f88621b56e88c9b60e90ae957b3de88b96315bc9
tainafernandes <tainafefe@gmail.com>
2023-01-17 12:08:00 -0300

    method postimplementing

---
+++ mscreditappraiser/src/main/java/io/github/tainafernandes/mscreditappraiser/domain/model/EvaluationData.java
@@ -0,0 +1,9 @@
+package io.github.tainafernandes.mscreditappraiser.domain.model;
+
+import lombok.Data;
+
+@Data
+public class EvaluationData {
+    private String cpf;
+    private String income;
+}
